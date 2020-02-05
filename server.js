affee6387834719e202d3006eb8ef8193dffb6c7
Redzuan <rabdulr@icloud.com>
2020-02-04 21:04:49 -0800

    Additions

--- server.js
+++ server.js
@@ -45,10 +45,45 @@ const addGuest = (guest)=> {
       return guest;
     });
 };
-// const server = 
-http.createServer((req, response) => {
-    
-    response.write("hello world")
-    response.end()
 
-}).listen(3000)
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res)=> {
+    if(req.url === '/'){
+        readFile('./index.html')
+            .then( html => {
+                res.write(html);
+                res.end()
+            })
+    }
+    else if(req.url === '/api/guests'){
+        if(req.method === 'GET'){
+            readFile('./guests.json')
+                .then( html => {
+                    res.write(html);
+                    res.end();
+                })
+        }
+        else if(req.method === 'POST'){
+            let buffer = '';
+            req.on('data', (chunk)=>{
+                buffer += chunk;
+            });
+            req.on('end', ()=> {
+                const guest = JSON.parse(buffer)
+                addGuest(guest)
+                    .then( g => {
+                        res.write(JSON.stringify(g));
+                        res.end();
+                    })
+            })
+        }
+    }
+    else {
+        res.statusCode = 404;
+        res.write('');
+        res.end();
+    }
+})
+
+server.listen(port, () => console.log(`Listening on ${ port }`))
